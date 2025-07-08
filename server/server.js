e33ddc67c490b3bd62a453b31e11ea740cd4c318
Pavithra K R <pavithra922710@gmail.com>
2025-07-08 20:27:27 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,7 +31,12 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
-  cookie: { maxAge: 1000 * 60 * 60 * 24 }
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none'
+  }
 }));
 
 // MONGODB CONNECTION
