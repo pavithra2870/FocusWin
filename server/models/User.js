a3a77bb116d46544648e03094bc0c4b4f1a96c0d
whowhy1526 <purushothaman1526@gmail.com>
2025-07-09 14:25:43 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  email: { 
-    type: String, 
-    required: true, 
+  username: {
+    type: String,
+    required: true,
     unique: true,
-    lowercase: true,
     trim: true
   },
   name: {
@@ -13,8 +12,8 @@ const userSchema = new mongoose.Schema({
     required: true,
     trim: true
   },
-  password: { 
-    type: String, 
+  password: {
+    type: String,
     required: true,
     minlength: 6
   },
